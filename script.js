5f2c5e2b3235707e94ae2eb4f69fa96c92bd0ef2
soumen bhunia <sob99338@gmail.com>
2023-10-21 00:03:17 +0530

    image added

---
+++ script.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".nav");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 20) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
